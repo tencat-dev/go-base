afc9dc0d1c1c527aad263787bbf6a12b272ba35b
anhnmt <42261776+anhnmt@users.noreply.github.com>
2026-01-21 21:07:05 +0700

    build(devenv): update development environment setup with essential Go tools
    Add multiple Go CLI tools for development workflow including migrate,
    protobuf generators, Kratos framework tools, Wire DI, and Buf for
    protocol buffer management
    
    BREAKING CHANGE: script name changed from 'hello' to 'init'

--- devenv.nix
+++ devenv.nix
@@ -20,14 +20,21 @@
   # services.postgres.enable = true;
 
   # https://devenv.sh/scripts/
-  scripts.hello.exec = ''
-    echo hello from $GREET
+  scripts.init.exec = ''
+    go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
+    go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+		go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+		go install github.com/go-kratos/kratos/cmd/kratos/v2@latest
+		go install github.com/go-kratos/kratos/cmd/protoc-gen-go-http/v2@latest
+		go install github.com/google/gnostic/cmd/protoc-gen-openapi@latest
+		go install github.com/google/wire/cmd/wire@latest
+		go install github.com/stephenafamo/bob/gen/bobgen-psql@latest
+		go install github.com/bufbuild/buf/cmd/buf@latest
   '';
 
   # https://devenv.sh/basics/
   enterShell = ''
-    hello         # Run scripts directly
-    git --version # Use packages
+    init         # Run scripts directly
   '';
 
   # https://devenv.sh/tasks/
